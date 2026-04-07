f748b7c68aa33cc921eee40d65eac6187612d62c
logr455 <lorenzogreco455@gmail.com>
2026-04-08 01:05:57 +0200

    prova_con_maze_res

--- merda.py
+++ merda.py
@@ -205,6 +205,38 @@ class MazeGenerator:
 
         return grid
 
+
+def get_open_neighbours(grid: list[list[int]], x: int, y: int, visited: set) -> list[tuple[int, int, str]]:
+    neighbours = []
+    for direction, values in DIRECTIONS.items():
+        if not grid[y][x] & values["num"]:
+            nx, ny = x + values["dx"], y + values["dy"]
+            if (nx, ny) not in visited:
+                neighbours.append((nx, ny, direction))
+    return neighbours
+
+
+def maze_res(maze: MazeGenerator, grid: list[list[int]]) -> list[tuple[int, int, str]]:
+    visited = set()
+    stack = []
+    current_x, current_y = maze.entry
+    visited.add((current_x, current_y))
+
+    while (current_x, current_y) != maze.exit:
+        neighbours = get_open_neighbours(grid, current_x, current_y, visited)
+
+        if neighbours:
+            nx, ny, direction = neighbours[0]
+            stack.append((current_x, current_y, direction))
+            visited.add((nx, ny))
+            current_x, current_y = nx, ny
+        else:
+            if not stack:
+                return []
+            current_x, current_y, _ = stack.pop()
+
+    return stack
+
 def crea_pezzi_cella(valore_cella):
     RESET = "\033[0m"
     MURO = "\033[96m" + "█" + RESET    
